1553721b52ece8f912298324de1cb2724eeee4a0
MrOld <cioccopesce43@gmail.com>
2026-09-22 17:21:10 +0200

    first sample commit

---
+++ punch.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import ssl
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=443,ssl_context=("cert.pem","key.pem"))
